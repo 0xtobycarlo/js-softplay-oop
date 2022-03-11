87112fcae12c79754b36d66df096a810fa242ee9
Toby Carlo <98287394+0xtobycarlo@users.noreply.github.com>
2022-03-11 11:13:50 +0000

    tests passed

--- src/soft-play-oop.js
+++ src/soft-play-oop.js
@@ -1,24 +1,13 @@
-// TODO: Write your class in this file
 class SoftPlay {
   constructor() {
     this.adults = 0;
     this.children = 0;
   }
-
-  /**
-   * Method to check current occupancy.
-   * @returns Numbers of adults and children inside the play center.
-   */
+  
   occupancy() {
     return { adults: this.adults, children: this.children };
   }
 
-  /**
-   * Updates current occupancy based on arriving groups.
-   * @param {integer} numAdults Number of adults in the arriving group.
-   * @param {integer} numChildren Number of children in the arriving group.
-   * @returns True if the group is allowed in or false if entry is denied.
-   */
   enter(numAdults, numChildren) {
     const isAccompanied = numAdults;
     if (isAccompanied) {
@@ -29,12 +18,6 @@ class SoftPlay {
     return false;
   }
 
-  /**
-   * Updates current occupancy based on leaving groups.
-   * @param {integer} numAdults
-   * @param {integer} numChildren
-   * @returns True if the group is allowed out or false if exit is denied.
-   */
   leave(numAdults, numChildren) {
     const isAccompanied = numAdults;
     if (numAdults > this.adults || numChildren > this.children) {
@@ -57,7 +40,6 @@ class SoftPlay {
   }
 }
 
-// TODO: Change undefined to the name of your class
 module.exports = {
   SoftPlay: SoftPlay,
 };
